0431d7b428b47879002b9a1f1f288a682996a1c0
Yasin Shah <yas1nshah@yahoo.com>
2024-05-04 18:20:07 +0500

    6

--- lib/foramt-price.ts
+++ lib/foramt-price.ts
@@ -1,18 +1,22 @@
 function formatAmount(amount: number) {
-    if (amount >= 10000000) {
-      // Amount is 1 Crore or more
-      const crorePart = Math.floor(amount / 10000000);
-  
-      return `${crorePart} Crores`;
-    } else if (amount >= 100000) {
-      // Amount is 1 Lakh or more
-      const lakhPart = Math.floor(amount / 100000);
-  
-      return `${lakhPart} Lacs`;
-    } else {
-      // Amount is less than 1 Lakh
-      const formattedAmount = Math.floor(amount); // Remove decimal part
-      return `${formattedAmount} Rupees`;
-    }
+  if (amount >= 10000000) {
+    // Amount is 1 Crore or more
+    const crorePart = Math.floor(amount / 10000000);
+    const remainder = amount % 10000000;
+    const lakhPart = remainder >= 100000 ? Math.floor(remainder / 100000) : 0;
+    const formattedAmount = lakhPart > 0 ? `${crorePart},${lakhPart} Crores` : `${crorePart} Crores`;
+    return formattedAmount;
+  } else if (amount >= 100000) {
+    // Amount is 1 Lakh or more
+    const lakhPart = Math.floor(amount / 100000);
+    const remainder = amount % 100000;
+    const formattedAmount = remainder > 0 ? `${lakhPart}.${remainder} Lakh` : `${lakhPart} Lakh`;
+    return formattedAmount;
+  } else {
+    // Amount is less than 1 Lakh
+    const formattedAmount = Math.floor(amount); // Remove decimal part
+    return `${formattedAmount} Rupees`;
   }
-  export default formatAmount
+}
+
+export default formatAmount;
